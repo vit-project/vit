2d0a87758742eb69e93aab0c85277659b0e4538b
Scott Kostyshak <skostysh@princeton.edu>
2013-09-15 01:30:15 -0400

    Implement support for key codes
    Key codes, such as <F1>, <End>, <Return> can now be used
    in keybindings (both for the key and for the command).
    Most of these are the same as the codes used in Vim.

--- vitrc.pl
+++ vitrc.pl
@@ -18,14 +18,58 @@ sub parse_vitrc {
           exit(1);
         }
 
+        $skey = &replace_keycodes("$skey");
+        $cmd = &replace_keycodes("$cmd");
+
         $skey = eval "\"$skey\"";
-        $cmd =~ s/<Return>/\n/g;
 
         $shortcuts{$skey} = $cmd;
       }
     }
     close(IN);
   }
+}
+
+#------------------------------------------------------------------
+
+sub replace_keycodes {
+  my $str_ = $_[0];
+
+  $str_ =~ s/<F1>/KEY_F(1)/e;
+  $str_ =~ s/<F2>/KEY_F(2)/e;
+  $str_ =~ s/<F3>/KEY_F(3)/e;
+  $str_ =~ s/<F4>/KEY_F(4)/e;
+  $str_ =~ s/<F5>/KEY_F(5)/e;
+  $str_ =~ s/<F6>/KEY_F(6)/e;
+  $str_ =~ s/<F7>/KEY_F(7)/e;
+  $str_ =~ s/<F8>/KEY_F(8)/e;
+  $str_ =~ s/<F9>/KEY_F(9)/e;
+  $str_ =~ s/<F10>/KEY_F(10)/e;
+  $str_ =~ s/<F11>/KEY_F(11)/e;
+  $str_ =~ s/<F12>/KEY_F(12)/e;
+
+  $str_ =~ s/<Home>/KEY_HOME/e;
+  $str_ =~ s/<End>/KEY_END/e;
+
+  $str_ =~ s/<Insert>/KEY_IC/e;
+  $str_ =~ s/<Del>/KEY_DC/e;
+
+  $str_ =~ s/<PageUp>/KEY_PPAGE/e;
+  $str_ =~ s/<PageDown>/KEY_NPAGE/e;
+
+  $str_ =~ s/<Up>/KEY_UP/e;
+  $str_ =~ s/<Down>/KEY_DOWN/e;
+  $str_ =~ s/<Right>/KEY_RIGHT/e;
+  $str_ =~ s/<Left>/KEY_LEFT/e;
+
+  $str_ =~ s/<Backspace>/KEY_BACKSPACE/e;
+
+  # We don't evaluate these ones (no 'e').
+  $str_ =~ s/<Space>/ /;
+  $str_ =~ s/<Tab>/\t/;
+  $str_ =~ s/<Return>/\n/;
+
+  return $str_;
 }
 
 return 1;
