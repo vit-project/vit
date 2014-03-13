20cbaa8809012969035327bacb2c87477f6c3c6b
Scott Kostyshak <skostysh@princeton.edu>
2014-03-13 01:07:45 -0400

    Remove some hardcodings of colors for tmux
    Thanks to Devendra Ghate

--- curses.pl
+++ curses.pl
@@ -8,11 +8,11 @@ sub init_curses {
   curs_set(0);
   start_color();
   use_default_colors();
-  init_pair($COLOR_ERRORS,231,1); # white on red # FIXME xterm specific, wrong for ANSI
+  init_pair($COLOR_ERRORS,COLOR_WHITE,COLOR_RED);
   if ( $m eq 'init' ) {
-    init_pair($COLOR_SELECTION,231,4); # white on blue  # FIXME xterm specific, wrong for ANSI
+    init_pair($COLOR_SELECTION,COLOR_WHITE,COLOR_BLUE);
   }
-  init_pair($COLOR_EMPTY_LINE,4,-1); # blue foreground
+  init_pair($COLOR_EMPTY_LINE,COLOR_BLUE,-1); # blue foreground
   my $HEADER_SIZE = 3;
   $REPORT_LINES = $LINES - $HEADER_SIZE - 1;
   $REPORT_COLS = $COLS - 2;
