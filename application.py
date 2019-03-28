944aa8cf4617d262c548a7166c0cd733a5d7ba90
Chad Phillips <chad@apartmentlines.com>
2019-03-28 11:16:14 -0700

    view/edit tasks

--- application.py
+++ application.py
@@ -48,17 +48,34 @@ clear_screen_seq = curses.tigetstr('clear') or b''
 def clear_screen():
     os.write(sys.stdout.fileno(), e3_seq + clear_screen_seq)
 
+class TaskRow():
+    def __init__(self, task):
+        self.task = task
+        self.uuid = self.task['uuid']
+        self.columns = self.build_columns()
+
+    def build_columns(self):
+        width = get_app_width()
+        desc_width = get_app_width() - 6
+        columns = [
+          (6, str(self.task['id'])),
+          (60, self.task['description']),
+          (10, task_date(self.task, 'scheduled')),
+        ]
+        return columns
+
+
 class SelectableRow(urwid.WidgetWrap):
     """Wraps 'urwid.Columns' to make it selectable.
     This class has been slightly modified, but essentially corresponds to this class posted on stackoverflow.com:
     https://stackoverflow.com/questions/52106244/how-do-you-combine-multiple-tui-forms-to-write-more-complex-applications#answer-52174629"""
 
-    def __init__(self, contents, *, align="left", on_select=None, space_between=2):
-        pf.pprint(contents)
+    def __init__(self, task_row, *, align="left", on_select=None, space_between=2):
         # A list-like object, where each element represents the value of a column.
-        self.contents = contents
+        self.task = task_row.task
+        self.uuid = task_row.uuid
 
-        self._columns = urwid.Columns([(w, urwid.Text(c, align=align)) for (w, c) in contents],
+        self._columns = urwid.Columns([(w, urwid.Text(c, align=align)) for (w, c) in task_row.columns],
                                        dividechars=space_between)
         self._focusable_columns = urwid.AttrMap(self._columns, '', 'reveal focus')
 
@@ -76,10 +93,8 @@ class SelectableRow(urwid.WidgetWrap):
         return True
 
     def keypress(self, size, key):
-        if self.on_select and key in ('enter',):
-            self.on_select(self)
-            key = None
-
+        if self.on_select:
+            key = self.on_select(self, size, key)
         return key
 
     def set_contents(self, contents):
@@ -100,12 +115,13 @@ def task_date(task, attr, fmt='%Y-%m-%d'):
     return ''
 
 class TaskModel(object):
-    def __init__(self, reports, report):
+    def __init__(self, reports, report=None):
 
         self.current_task_id = None
         self.reports = reports
         self.report = report
-        self.tasks = []
+        if report:
+            self.update_report(report)
 
     def add(self, contact):
         pass
@@ -118,61 +134,63 @@ class TaskModel(object):
         tasks = tw.tasks.filter(*self.active_report()['filter'])
         self.tasks = tasks
 
-    def build_task_row(self, task):
-        width = get_app_width()
-        desc_width = get_app_width() - 6
-        columns = [
-          (6, str(task['id'])),
-          (60, task['description']),
-          (10, task_date(task, 'scheduled')),
-        ]
-        return columns
-
-    def get_summary(self, report=None):
-        report = report or self.report
-        self.update_report(report)
-        summary = []
-        for task in self.tasks:
-            summary.append(self.build_task_row(task))
-        return summary
-
+#    def get_summary(self, report=None):
+#        report = report or self.report
+#        self.update_report(report)
+#        summary = []
+#        for task in self.tasks:
+#            summary.append(self.build_task_row(task))
+#        return summary
+#
 #    def _reload_list(self, new_value=None):
 #        self._list_view.options = self._model.get_summary()
 #        self._list_view.value = new_value
+
+def user_output(command_args, confirm="Press Enter to continue...", clear=True):
+    if clear:
+        clear_screen()
+    output = subprocess.check_output(command_args)
+    print(output.decode("utf-8"))
+    if confirm:
+        try:
+            input(confirm)
+        except:
+            raw_input(confirm)
+    if clear:
+        clear_screen()
 
 def init_app(reports, report):
 
     def key_pressed(key):
         if key in ('q', 'Q', 'esc'):
             raise urwid.ExitMainLoop()
-        elif key == 'e':
+
+    def on_select(row, size, key):
+        if key == 'e':
             loop.stop()
             clear_screen()
-            subprocess.call(["task", "1", "edit"])
+            subprocess.run(["task", row.uuid, "edit"])
             clear_screen()
             loop.start()
+            key = None
         elif key == 'enter':
             loop.stop()
-            clear_screen()
-            subprocess.call(["task", "1", "info"])
-            try:
-                input("Press Enter to continue...")
-            except:
-                raw_input("Press Enter to continue...")
-            clear_screen()
+            user_output(["task", row.uuid, "info"])
             loop.start()
+            key = None
+        return key
 
-    tasks = TaskModel(reports, report)
+    model = TaskModel(reports, report)
 
     #titles=self._model.active_report()['labels'],
     contents = []
-    contents.append(SelectableRow([
-        (6, "ID"),
-        (60, "Description"),
-        (10, "Starts"),
-    ]))
-    for task in tasks.get_summary():
-      contents.append(SelectableRow(task))
+    #contents.append(SelectableRow([
+    #    (6, "ID"),
+    #    (60, "Description"),
+    #    (10, "Starts"),
+    #]))
+    for task in model.tasks:
+        contents.append(SelectableRow(TaskRow(task), on_select=on_select))
 
     header = urwid.Text('Welcome to PYT')
     footer = urwid.Text('Status: ready')
