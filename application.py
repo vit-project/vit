087a72d5a77377541a434faa39be2166ec6d7f52
Chad Phillips <chad@apartmentlines.com>
2019-03-27 22:52:37 -0700

    initial urwid structure

--- application.py
+++ application.py
@@ -1,10 +1,32 @@
-from asciimatics.widgets import Frame, MultiColumnListBox, Layout, Divider, Text, \
-    Button, TextBox, Widget, Label
-from asciimatics.scene import Scene
-from asciimatics.screen import Screen
-from asciimatics.exceptions import ResizeScreenError, NextScene, StopApplication
-import sys
+#!/usr/bin/env python
+#
+# Urwid example lazy directory browser / tree view
+#    Copyright (C) 2004-2011  Ian Ward
+#    Copyright (C) 2010  Kirk McDonald
+#    Copyright (C) 2010  Rob Lanphier
+#
+#    This library is free software; you can redistribute it and/or
+#    modify it under the terms of the GNU Lesser General Public
+#    License as published by the Free Software Foundation; either
+#    version 2.1 of the License, or (at your option) any later version.
+#
+#    This library is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#    Lesser General Public License for more details.
+#
+#    You should have received a copy of the GNU Lesser General Public
+#    License along with this library; if not, write to the Free Software
+#    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#
+# Urwid web site: http://excess.org/urwid/
+
+from __future__ import print_function
+import subprocess
+
 import re
+import urwid
+
 from functools import partial
 from tasklib import TaskWarrior
 tw = TaskWarrior()
@@ -14,6 +36,52 @@ import pprint
 pp = pprint.PrettyPrinter()
 pf = pprint.PrettyPrinter(stream=open("/tmp/test",'w'))
 
+PALETTE = [
+    ('reveal focus', 'black', 'dark cyan', 'standout'),
+]
+
+class SelectableRow(urwid.WidgetWrap):
+    """Wraps 'urwid.Columns' to make it selectable.
+    This class has been slightly modified, but essentially corresponds to this class posted on stackoverflow.com:
+    https://stackoverflow.com/questions/52106244/how-do-you-combine-multiple-tui-forms-to-write-more-complex-applications#answer-52174629"""
+
+    def __init__(self, contents, *, align="left", on_select=None, space_between=2):
+        pf.pprint(contents)
+        # A list-like object, where each element represents the value of a column.
+        self.contents = contents
+
+        self._columns = urwid.Columns([(w, urwid.Text(c, align=align)) for (w, c) in contents],
+                                       dividechars=space_between)
+        self._focusable_columns = urwid.AttrMap(self._columns, '', 'reveal focus')
+
+        # Wrap 'urwid.Columns'.
+        super().__init__(self._focusable_columns)
+
+        # A hook which defines the behavior that is executed when a specified key is pressed.
+        self.on_select = on_select
+
+    def __repr__(self):
+        return "{}(contents='{}')".format(self.__class__.__name__,
+                                          self.contents)
+
+    def selectable(self):
+        return True
+
+    def keypress(self, size, key):
+        if self.on_select and key in ('enter',):
+            self.on_select(self)
+            key = None
+
+        return key
+
+    def set_contents(self, contents):
+        # Update the list record inplace...
+        self.contents[:] = contents
+
+        # ... and update the displayed items.
+        for t, (w, _) in zip(contents, self._columns.contents):
+            w.set_text(t)
+
 def get_app_width():
   return 80
 
@@ -22,12 +90,6 @@ def task_date(task, attr, fmt='%Y-%m-%d'):
     return task[attr].strftime(fmt)
   except AttributeError:
     return ''
-
-def wrap_line(text, length=40):
-  output = ''
-  for x in text.splitlines():
-    output += '\n'.join(line.strip() for line in re.findall(r".{1,%s}(?:\s+|$)" % length, x))
-  return output
 
 class TaskModel(object):
     def __init__(self, reports, report):
@@ -52,11 +114,11 @@ class TaskModel(object):
         width = get_app_width()
         desc_width = get_app_width() - 6
         columns = [
-          str(task['id']),
-          wrap_line(task['description'], desc_width - 6),
-          task_date(task, 'scheduled'),
+          (6, str(task['id'])),
+          (60, task['description']),
+          (10, task_date(task, 'scheduled')),
         ]
-        return (columns, task['id'])
+        return columns
 
     def get_summary(self, report=None):
         report = report or self.report
@@ -66,57 +128,47 @@ class TaskModel(object):
             summary.append(self.build_task_row(task))
         return summary
 
-class ListView(Frame):
-    def __init__(self, screen, model):
-        super(ListView, self).__init__(screen,
-                                       screen.height,
-                                       screen.width,
-                                       on_load=self._reload_list,
-                                       hover_focus=True,
-                                       has_border=False,
-                                       )
-        self._model = model
-
-        # Create the form for displaying the list of tasks.
-        width = get_app_width()
-        desc_width = get_app_width() - 6
-        self._list_view = MultiColumnListBox(
-            Widget.FILL_FRAME,
-            [6, desc_width - 2, 11],
-            self._model.get_summary(),
-            name="tasks",
-            titles=self._model.active_report()['labels'],
-            on_change=self._on_pick)
-        layout = Layout([100], fill_frame=True)
-        self.add_layout(layout)
-        layout.add_widget(self._list_view)
-        self.fix()
-        self._on_pick()
-
-    def _on_pick(self):
-        pass
-
-    def _reload_list(self, new_value=None):
-        self._list_view.options = self._model.get_summary()
-        self._list_view.value = new_value
-
-    @staticmethod
-    def _quit():
-        raise StopApplication("User pressed quit")
-
-def task_report(tasks, screen, scene):
-    scenes = [
-        Scene([ListView(screen, tasks)], -1, name="Tasks"),
-    ]
-    screen.play(scenes, stop_on_resize=True, start_scene=scene)
-
+#    def _reload_list(self, new_value=None):
+#        self._list_view.options = self._model.get_summary()
+#        self._list_view.value = new_value
 
 def init_app(reports, report):
+
+    def key_pressed(key):
+        if key in ('q', 'Q', 'esc'):
+            raise urwid.ExitMainLoop()
+        elif key == 'e':
+            loop.stop()
+            subprocess.call(["clear"])
+            subprocess.call(["task", "1", "edit"])
+            loop.start()
+        elif key == 'enter':
+            loop.stop()
+            subprocess.call(["clear"])
+            subprocess.call(["task", "1", "info"])
+            loop.start()
+
     tasks = TaskModel(reports, report)
-    last_scene = None
-    while True:
-        try:
-            Screen.wrapper(partial(task_report, tasks), catch_interrupt=False, arguments=[last_scene])
-            sys.exit(0)
-        except ResizeScreenError as e:
-            last_scene = e.scene
+
+    #titles=self._model.active_report()['labels'],
+    contents = []
+    contents.append(SelectableRow([
+        (6, "ID"),
+        (60, "Description"),
+        (10, "Starts"),
+    ]))
+    for task in tasks.get_summary():
+      contents.append(SelectableRow(task))
+
+    header = urwid.Text('Welcome to PYT')
+    footer = urwid.Text('Status: ready')
+
+    listbox = urwid.ListBox(urwid.SimpleFocusListWalker(contents))
+
+    widget = urwid.Frame(
+        listbox,
+        header=header,
+        footer=footer,
+    )
+    loop = urwid.MainLoop(widget, PALETTE, unhandled_input=key_pressed)
+    loop.run()
