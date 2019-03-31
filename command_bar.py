93b7e6236d4dd397dbae6770acfe8cba58edd029
Chad Phillips <chad@apartmentlines.com>
2019-03-31 00:09:05 -0700

    set/get metadata on command bar

--- command_bar.py
+++ command_bar.py
@@ -5,6 +5,7 @@ class CommandBar(urwid.Edit):
     """
     def __init__(self, **kwargs):
         self.event = kwargs['event']
+        self.metadata = None
         kwargs.pop('event')
         return super().__init__(**kwargs)
 
@@ -13,10 +14,22 @@ class CommandBar(urwid.Edit):
         """
         # TODO: Readline edit shortcuts.
         if key in ('enter', 'esc'):
-            data = self.get_edit_text()
+            text = self.get_edit_text()
+            data = {
+                'key': key,
+                'text': text,
+                'metadata': self.get_metadata(),
+            }
             self.set_caption('')
             self.set_edit_text('')
-            self.event.emit('command-bar:keypress', {'key': key, 'data': data})
+            self.set_metadata(None)
+            self.event.emit('command-bar:keypress', data)
             return True
         return super().keypress(size, key)
+
+    def get_metadata(self):
+        return self.metadata.copy() if self.metadata else None
+
+    def set_metadata(self, metadata):
+        self.metadata = metadata
 
