181288ccf8921b7efa016eb469caaf94b1cf1576
Chad Phillips <chad@apartmentlines.com>
2019-04-15 11:26:25 -0700

    combined formatter for description

--- formatter/description.py
+++ formatter/description.py
@@ -1,4 +1,17 @@
 from formatter import String
 
 class Description(String):
-    pass
+    def format(self, description, task):
+        if task['annotations']:
+            return self.format_combined(description, task)
+        else:
+            return description
+
+    def format_combined(self, description, task):
+        return '%s\n%s' % (description, self.format_annotations(task))
+
+    def format_annotations(self, task):
+        return "\n".join([self.format_annotation(a) for a in task['annotations']])
+
+    def format_annotation(self, annotation):
+        return '  %s %s' % (annotation['entry'].strftime(self.defaults.annotation), annotation['description'])
