76918eb466e8013bc4270f67d63dc09dfb88665a
Chad Phillips <chad@apartmentlines.com>
2019-04-29 20:41:54 -0700

    util method to load list of udas

--- uda.py
+++ uda.py
@@ -3,3 +3,9 @@ def get(name, task_config):
     if 'uda' in subtree and name in subtree['uda']:
         return subtree['uda'][name]
     return None
+
+def get_configured(task_config):
+    subtree = task_config.subtree(r'^uda\.', walk_subtree=False)
+    if 'uda' in subtree:
+        return list(subtree['uda'].keys())
+    return []
