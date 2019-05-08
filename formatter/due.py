f201888373a460ef52697b3eca891a6d595ea9ed
Chad Phillips <chad@apartmentlines.com>
2019-05-07 19:20:11 -0700

    better var name

--- formatter/due.py
+++ formatter/due.py
@@ -1,8 +1,8 @@
 from formatter import DateTime
 
 class Due(DateTime):
-    def get_due_state(self, dt):
-        return self.defaults.get_due_state(dt)
+    def get_due_state(self, due):
+        return self.defaults.get_due_state(due)
 
-    def colorize(self, dt):
-        return self.colorizer.due(self.get_due_state(dt))
+    def colorize(self, due):
+        return self.colorizer.due(self.get_due_state(due))
