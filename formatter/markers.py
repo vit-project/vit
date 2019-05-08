d9efeb045d5265f5abda62c8d4e1ffcb30af820b
Chad Phillips <chad@apartmentlines.com>
2019-05-07 19:27:58 -0700

    markers for due/due.today/overdue

--- formatter/markers.py
+++ formatter/markers.py
@@ -8,6 +8,8 @@ class Markers(Marker):
             width, text_markup = self.format_tags(width, text_markup, task['tags'])
         if self.mark_project:
             width, text_markup = self.format_project(width, text_markup, task['project'])
+        if self.mark_due and task['due']:
+            width, text_markup = self.format_due(width, text_markup, task['due'])
         for uda_name, uda_type in self.udas.items():
             if getattr(self, 'mark_%s' % uda_name):
                 width, text_markup = self.format_uda(width, text_markup, uda_name, uda_type, task[uda_name])
@@ -51,6 +53,14 @@ class Markers(Marker):
             custom_label = 'project.%s.label' % project
             label = self.labels['project.label'] if not custom_label in self.labels else self.labels[custom_label]
             return self.add_label(color, label, width, text_markup)
+
+    def format_due(self, width, text_markup, due):
+        due_state = self.defaults.get_due_state(due)
+        if due_state:
+            color = self.colorizer.due(due_state)
+            label = self.labels['%s.label' % due_state]
+            return self.add_label(color, label, width, text_markup)
+        return width, text_markup
 
     def format_uda(self, width, text_markup, uda_name, uda_type, value):
         if not value:
