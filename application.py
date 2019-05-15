12c2937a5a7e6153c90bc578743776f4de093100
Chad Phillips <chad@apartmentlines.com>
2019-05-15 16:08:47 -0700

    initial CLI options parsing

--- application.py
+++ application.py
@@ -20,6 +20,7 @@ from process import Command
 from task import TaskListModel, TaskAutoComplete
 from keybinding_parser import KeybindingParser
 from key_cache import KeyCache
+from actions import Actions
 from markers import Markers
 from color import TaskColorConfig, TaskColorizer
 import version
@@ -89,7 +90,8 @@ class Application():
         self.extra_filters = []
         self.search_term_active = ''
         self.action_registry = ActionRegistry()
-        self.register_actions()
+        self.actions = Actions(self.action_registry)
+        self.actions.register()
         self.keybinding_parser = KeybindingParser(self.loader, self.config, self.action_registry)
         self.setup_keybindings()
         self.action_manager = ActionManagerRegistry(self.action_registry, self.key_cache.keybindings, event=self.event)
@@ -114,48 +116,6 @@ class Application():
     def setup_config(self):
         self.confirm = self.config.confirmation_enabled
         self.wait = self.config.wait_enabled
-
-    def register_actions(self):
-        self.action_registrar = self.action_registry.get_registrar()
-        # Global.
-        self.action_registrar.register('QUIT', 'Quit the application')
-        self.action_registrar.register('QUIT_WITH_CONFIRM', 'Quit the application, after confirmation')
-        self.action_registrar.register('REFRESH', 'Refresh the current report')
-        self.action_registrar.register('TASK_ADD', 'Add a task')
-        self.action_registrar.register('REPORT_FILTER', 'Filter current report')
-        self.action_registrar.register('TASK_UNDO', 'Undo last task change')
-        self.action_registrar.register('COMMAND_BAR_EX', "Open the command bar in 'ex' mode")
-        self.action_registrar.register('COMMAND_BAR_EX_TASK_READ_WAIT', "Open the command bar in 'ex' mode with '!rw task ' appended")
-        self.action_registrar.register('COMMAND_BAR_SEARCH_FORWARD', 'Open the command bar in search forward mode')
-        self.action_registrar.register('COMMAND_BAR_SEARCH_REVERSE', 'Open the command bar in search reverse mode')
-        self.action_registrar.register('COMMAND_BAR_SEARCH_NEXT', 'Search next')
-        self.action_registrar.register('COMMAND_BAR_SEARCH_PREVIOUS', 'Search previous')
-        self.action_registrar.register('GLOBAL_ESCAPE', 'Top-level escape function')
-        self.action_registrar.register(self.action_registry.noop_action_name, 'Used to disable a default keybinding action')
-        # List.
-        self.action_registrar.register('LIST_UP', 'Move list focus up one entry')
-        self.action_registrar.register('LIST_DOWN', 'Move list focus down one entry')
-        self.action_registrar.register('LIST_PAGE_UP', 'Move list focus up one page')
-        self.action_registrar.register('LIST_PAGE_DOWN', 'Move list focus down one page')
-        self.action_registrar.register('LIST_HOME', 'Move list focus to top of the list')
-        self.action_registrar.register('LIST_END', 'Move list focus to bottom of the list')
-        self.action_registrar.register('LIST_SCREEN_TOP', 'Move list focus to top of the screen')
-        self.action_registrar.register('LIST_SCREEN_MIDDLE', 'Move list focus to middle of the screen')
-        self.action_registrar.register('LIST_SCREEN_BOTTOM', 'Move list focus to bottom of the screen')
-        self.action_registrar.register('LIST_FOCUS_VALIGN_CENTER', 'Move focused item to center of the screen')
-        # Task.
-        self.action_registrar.register('TASK_ANNOTATE', 'Add an annotation to a task')
-        self.action_registrar.register('TASK_DELETE', 'Delete task')
-        self.action_registrar.register('TASK_DENOTATE', 'Denotate a task')
-        self.action_registrar.register('TASK_MODIFY', 'Modify task')
-        self.action_registrar.register('TASK_START_STOP', 'Start/stop task')
-        self.action_registrar.register('TASK_DONE', 'Mark task done')
-        self.action_registrar.register('TASK_PRIORITY', 'Modify task priority')
-        self.action_registrar.register('TASK_PROJECT', 'Modify task project')
-        self.action_registrar.register('TASK_TAGS', 'Modify task tags')
-        self.action_registrar.register('TASK_WAIT', 'Wait a task')
-        self.action_registrar.register('TASK_EDIT', 'Edit a task via the default editor')
-        self.action_registrar.register('TASK_SHOW', 'Show task details')
 
     def register_managed_actions(self):
         # Global.
