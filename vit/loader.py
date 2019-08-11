8bdc78511817926c376964fba8bd2f07603b2c5d
Jochen Sprickerhof <git@jochen.sprickerhof.de>
2019-08-11 12:36:24 +0200

    Add support for XDG Base Directory

--- vit/loader.py
+++ vit/loader.py
@@ -12,6 +12,21 @@ class Loader(object):
     def __init__(self):
         self.user_config_dir = os.path.expanduser('VIT_DIR' in env.user and env.user['VIT_DIR'] or DEFAULT_VIT_DIR)
 
+        if not os.path.exists(self.user_config_dir):
+            xdg_config_home = env.user.get("XDG_CONFIG_HOME") or os.path.join(
+                os.path.expanduser("~"), ".config"
+            )
+
+            xdg_config_dirs = [xdg_config_home] + (
+                env.user.get("XDG_CONFIG_DIRS") or "/etc/xdg"
+            ).split(":")
+
+            for config_dir in xdg_config_dirs:
+                path = os.path.join(config_dir, "vit")
+                if os.path.exists(path):
+                    self.user_config_dir = path
+                    break
+
     def load_user_class(self, module_type, module_name, class_name):
         module = '%s.%s' % (module_type, module_name)
         filepath = '%s/%s/%s.py' % (self.user_config_dir, module_type, module_name)
