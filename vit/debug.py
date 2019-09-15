ca8d0c0e471bc8c8e090c06e14f4ea2c6522b02f
Chad Phillips <chad@apartmentlines.com>
2019-09-15 15:40:05 -0600

    platform-independent temp dir, better debug file name

--- vit/debug.py
+++ vit/debug.py
@@ -1,7 +1,10 @@
 import pprint
+import tempfile
+
+LOG_FILE = '%s/%s' % (tempfile.gettempdir(), 'vit-debug.log')
 
 pp = pprint.PrettyPrinter()
-pf = pprint.PrettyPrinter(stream=open("/tmp/test",'w'))
+pf = pprint.PrettyPrinter(stream=open(LOG_FILE,'w'))
 
 def console(*args, **kwargs):
     pp.pprint(*args, **kwargs)
