faaca39e0dc36650c02cdf9a933378cbc0e8476f
Chad Phillips <chad@apartmentlines.com>
2020-07-29 13:51:01 -0700

    reference issue in todo

--- vit/formatter/uda_date.py
+++ vit/formatter/uda_date.py
@@ -10,6 +10,7 @@ class UdaDate(DateTime):
         if not dt:
             return self.markup_none(self.colorize())
         # TODO: Remove this once tasklib bug is fixed.
+        # https://github.com/robgolding/tasklib/issues/30
         dt = dt if isinstance(dt, datetime.datetime) else serializer.timestamp_deserializer(dt)
         formatted_date = dt.strftime(self.custom_formatter or self.formatter.report)
         return (len(formatted_date), (self.colorize(dt), formatted_date))
