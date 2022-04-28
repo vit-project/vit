9801fbe7eebcbaf005e757d6c5d46cc5a455b917
Chad Phillips <chad@apartmentlines.com>
2022-04-28 15:20:32 -0400

    example hook for intelligent VIT refresh
    Uses example vit-external-refresh.sh with pid_dir enabled, to auto-refresh
    the VIT interface when tasks are modified outside of VIT in TaskWarrior.
    
    Smart enough to not auto refresh when the command comes from VIT.

---
+++ scripts/hooks/on-exit-refresh-vit.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+# Auto-refreshes all open VIT interfaces on any modifications.
+# Looks for the special $IS_VIT_COMMAND environment variable,
+# and if found, skips the refresh.
+
+# Make sure this script is in your PATH and executable, modify as necessary.
+# Refer to the example at scripts/vit-external-refresh.sh
+REFRESH_SCRIPT="vit-external-refresh.sh"
+
+# Count the number of tasks modified
+n=0
+while read modified_task; do
+    n=$((${n} + 1))
+done
+
+if ((${n} > 0)); then
+  if [ -z "${IS_VIT_COMMAND}" ]; then
+    logger "Tasks modified outside of VIT: ${n}, refreshing"
+    ${REFRESH_SCRIPT}
+  fi
+fi
+
+exit 0
