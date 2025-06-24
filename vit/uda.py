98ad2785a58b21e291f10fcaf94533ffe5bbc1a7
Chad Phillips <chad@apartmentlines.com>
2025-06-24 12:39:53 -0400

    fix #368 default to string type for UDA if no type configured

--- vit/uda.py
+++ vit/uda.py
@@ -7,5 +7,5 @@ def get(name, task_config):
 def get_configured(task_config):
     subtree = task_config.subtree(r'^uda\.', walk_subtree=False)
     if 'uda' in subtree:
-        return {k:v['type'] for k, v in subtree['uda'].items()}
+        return {k: v['type'] if 'type' in v else 'string' for k, v in subtree['uda'].items()}
     return {}
